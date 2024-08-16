49c060361d4c1512cb3e4dcd680aa9d8ea53161b
Shichida.Masatsugu <shicchi.7913@icloud.com>
2024-08-16 21:23:26 +0900

    add loadCorporateType function fron yml file

--- src/main/kotlin/com/syamei/CorporateType.kt
+++ src/main/kotlin/com/syamei/CorporateType.kt
@@ -1,8 +1,26 @@
 package com.syamei
 
+import com.charleskorn.kaml.Yaml
+import kotlinx.serialization.Serializable
+import java.nio.file.Path
+import java.nio.file.Files
+
+@Serializable
 data class CorporateType(
     val full: String,
-    val kanjiShort: String,
+    val kanjiShort: String?,
     val kanaShort: String,
-    val special: String,
+    val special: String?,
 )
+
+@Serializable
+private data class CorporateTypesWrapper(
+    val corporateTypes: List<CorporateType>
+)
+
+fun loadCorporateTypes(): List<CorporateType> {
+    val yamlContent = Files.readString(Path.of("src/main/resources/data/corporate_types.yml"))
+    val corporateTypesWrapper = Yaml.default.decodeFromString(CorporateTypesWrapper.serializer(), yamlContent)
+
+    return corporateTypesWrapper.corporateTypes
+}
